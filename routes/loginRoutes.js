06a67d9c0875ae1556c3e7e6047a9fc0bbe54763
Kaiky Botelho de Faria <156143541+kaiky-botelho@users.noreply.github.com>
2024-11-04 10:28:02 -0300

    Update loginRoutes.js

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -6,40 +6,18 @@ const { Pool } = require('pg'); // Para conexão com PostgreSQL
 
 router.use(bodyParser.urlencoded({ extended: true }));
 
-// Rota para a página de login
+// Rota para a páginas de logins e inicial
 router.get('/', (req, res) => {
-    res.render('login');  
+    res.render('inicial');  
 });
 
-// Rota para processar o login
-router.post('/login', async (req, res) => {
-    const { documento, senha } = req.body;
-
-    // Verificar se o usuário é um doador
-    const doadorResult = await pool.query('SELECT * FROM cadastro_doador WHERE doador_documento = $1', [documento]);
-    const doador = doadorResult.rows[0];
-
-    if (doador && await bcrypt.compare(senha, doador.doador_senha)) {
-        req.session.userId = doador.id_doador;
-        req.session.role = 'doador';
-        return res.redirect('/home-doador'); // Redireciona para a home do doador
-    }
-
-    // Verificar se o usuário é um beneficiário
-    const beneficiarioResult = await pool.query('SELECT * FROM cadastro_beneficiario WHERE benef_documento = $1', [documento]);
-    const beneficiario = beneficiarioResult.rows[0];
-
-    if (beneficiario && await bcrypt.compare(senha, beneficiario.benef_senha)) {
-        req.session.userId = beneficiario.id_beneficiario;
-        req.session.role = 'beneficiario';
-        return res.redirect('/home-beneficiario'); // Redireciona para a home do beneficiário
-    }
-
-    // Se não for encontrado, retorna erro
-    res.status(401).send('Usuário ou senha inválidos.');
+router.get('/loginBenf', (req, res) => {
+    res.render('loginBenf');  
 });
 
-// Exports da rota
-module.exports = router;
+router.get('/loginDoador', (req, res) => {
+    res.render('loginDoador');  
+});
+
 
   module.exports = router;
