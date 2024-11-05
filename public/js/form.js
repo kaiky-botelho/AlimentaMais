56d1a6843e4b6344c0418afd80f2267764f1d378
Kaiky Botelho de Faria <156143541+kaiky-botelho@users.noreply.github.com>
2024-11-05 11:26:15 -0300

    Update form.js

--- public/js/form.js
+++ public/js/form.js
@@ -1,5 +1,4 @@
 //buscar cep dodador
-
 function buscaCep(cep) {
     fetch(`https://viacep.com.br/ws/${cep}/json/`)
         .then(response => {
@@ -11,10 +10,10 @@ function buscaCep(cep) {
         })
         .then(data => {
             console.log(data);
-            document.getElementById("benef_endereco").value = data.logradouro;
-            document.getElementById("benef_bairro").value = data.bairro;
-            document.getElementById("benef_cidade").value = data.localidade;
-            document.getElementById("benef_UF").value = data.uf;
+            document.getElementById("doador_endereco").value = data.logradouro;
+            document.getElementById("doador_bairro").value = data.bairro;
+            document.getElementById("doador_cidade").value = data.localidade;
+            document.getElementById("doador_UF").value = data.uf;
         })
         .catch(error => {
             console.log("Erro:", error);
@@ -55,10 +54,10 @@ function buscaCep(cep) {
         })
         .then(data => {
             console.log(data);
-            document.getElementById("doador_endereco").value = data.logradouro;
-            document.getElementById("doador_bairro").value = data.bairro;
-            document.getElementById("doador_cidade").value = data.localidade;
-            document.getElementById("doador_UF").value = data.uf;
+            document.getElementById("benef_endereco").value = data.logradouro;
+            document.getElementById("benef_bairro").value = data.bairro;
+            document.getElementById("benef_cidade").value = data.localidade;
+            document.getElementById("benef_UF").value = data.uf;
         })
         .catch(error => {
             console.log("Erro:", error);
@@ -70,37 +69,4 @@ function buscaCep(cep) {
 $('#benef_cep').mask('00000-000');
 $('#benef_documento').mask('000.000.000-00', {reverse: true});
 $('#benef_telefone').mask('(00) 0 0000-0000');
-
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.post('/submit', (req, res) => {
-    const selectedOptions = req.body.options; // Isso pode ser um array ou uma string
-    if (!selectedOptions) {
-        return res.send('Nenhuma opção selecionada');
-    }
-
-    // Se apenas uma opção for selecionada, selectedOptions será uma string
-    // Se várias opções forem selecionadas, será um array
-    const optionsToSave = Array.isArray(selectedOptions) ? selectedOptions : [selectedOptions];
-
-    // Aqui você deve salvar as opções no banco de dados
-    // Exemplo usando PostgreSQL:
-    const query = 'INSERT INTO sua_tabela (coluna) VALUES ($1)';
-    optionsToSave.forEach(option => {
-        // Executar a inserção para cada opção
-        // db.query é uma função que você deve definir para se conectar ao PostgreSQL
-        db.query(query, [option], (err) => {
-            if (err) {
-                console.error(err);
-                return res.status(500).send('Erro ao salvar no banco');
-            }
-        });
-    });
-
-    res.send('Opções salvas com sucesso!');
-});
 
