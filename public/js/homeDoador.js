1fb9c391d3c3403c2b7f8150703dc5317290c1de
Kaiky Botelho de Faria <156143541+kaiky-botelho@users.noreply.github.com>
2024-11-14 09:24:33 -0300

    Editar Informação do Doador

---
+++ public/js/homeDoador.js
@@ -0,0 +1,62 @@
+document.getElementById('profile').addEventListener('click', function() {
+    const perfil = document.getElementById('perfil');
+    perfil.classList.toggle('aparecer'); 
+});
+
+function mostrar(acao) {
+    let section = document.getElementById('resultado-mostrar');
+
+    if (acao === 'editar') {
+        section.style.display = 'flex';
+        section.innerHTML = `
+            <div class="informacao-section" id="informacao-section"></div>
+                <form action="" method="post">
+                    <div class="form">
+                        <h4>Editar Informações</h4>
+                        <div class="form-input">
+                            <input type="text" name="nome_razao" id="nome_razao" placeholder="Nome">
+                        </div>
+                        <div class="form-input">
+                            <input type="email" name="doador_email" id="doador_email" placeholder="Email">
+                        </div>
+                        <div class="form-input">
+                            <input type="text" name="doador_endereco" id="doador_endereco" placeholder="Endereço">
+                        </div>
+                        <div class="form-input">
+                            <div class="form-input-inner">
+                                <input type="text" name="doador_cidade" id="doador_cidade" placeholder="Cidade">
+                                <input type="text" name="doador_UF" id="doador_UF" placeholder="UF">
+                            </div>
+                        </div>
+                        <div class="form-input">
+                            <div class="form-input-inner">
+                                <input type="text" name="doador_bairro" id="doador_bairro" placeholder="Bairro">
+                                <input type="text" name="doador_cep" id="doador_cep" placeholder="CEP">
+                            </div>
+                        </div>
+                        <button><img src="/img/atualizar.svg" alt="">Atualizar</button>
+                    </div>
+                </form>
+            
+        `;
+
+        document.getElementById('informacao-section').addEventListener('click', function() {
+            section.style.display = 'none';  
+        });
+
+    } else if (acao === 'deletar') {
+        section.style.display = 'flex';
+
+        section.innerHTML = `        
+        <div class="informacao-section" id="informacao-section">     </div>
+        <div class="deletar-confirmacao">
+            <h4>DELETAR SUA CONTA</h4>
+            <p>Sua conta será deletada definitivamente</p>
+            <p>Deseja continuar?</p>
+            <button><img src="/img/trash.svg" alt="" srcset="">DELETAR</button>
+   `;
+
+        document.getElementById('informacao-section').addEventListener('click', function() {
+        section.style.display = 'none'; });
+    }
+}
