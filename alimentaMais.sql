eab64f652765918796823c81b55372deb488712d
Dudaa-Santos <marriduarda010@gmail.com>
2024-11-27 16:18:24 -0300

    add_column

--- alimentaMais.sql
+++ alimentaMais.sql
@@ -1,10 +1,10 @@
 CREATE TABLE cadastro_doador (
 	id_doador SERIAL NOT NULL PRIMARY KEY,
 	nome_razao VARCHAR(200),
-	doador_documento VARCHAR(14) NOT NULL UNIQUE,
-	doador_telefone VARCHAR(20) NOT NULL UNIQUE,
+  	doador_documento VARCHAR(14) NOT NULL UNIQUE,
+	doador_telefone VARCHAR(20),
 	doador_data_nasc DATE,
-	doador_email VARCHAR(100) NOT NULL UNIQUE,
+	doador_email VARCHAR(100),
 	doador_senha VARCHAR(60),
 	doador_cep VARCHAR(10),
 	doador_cidade VARCHAR(100),
@@ -22,9 +22,9 @@ CREATE TABLE cadastro_beneficiario (
 	id_beneficiario SERIAL NOT NULL PRIMARY KEY,
 	nome VARCHAR(200),
   	benef_documento VARCHAR(20) NOT NULL UNIQUE,
-	benef_telefone VARCHAR(20) NOT NULL UNIQUE,
+	benef_telefone VARCHAR(20),
 	benef_data_nasc DATE,
-	benef_email VARCHAR(100) NOT NULL UNIQUE,
+	benef_email VARCHAR(100),
 	benef_senha VARCHAR(60),
 	benef_cep VARCHAR(10),
 	benef_cidade VARCHAR(100),
@@ -43,7 +43,7 @@ CREATE TABLE solicitacao (
 	solicitacao_alimento VARCHAR(100),
 	solicitacao_qtd VARCHAR(50),
 	solicitacao_obs VARCHAR(100),
-	endereco_retirada VARCHAR(100),
+	solicitacao_entrega VARCHAR(50) CHECK (solicitacao_entrega IN ('Entregar pessoalmente', 'Retirar no endereço')), 
 	solicitacao_data DATE,
 	solicitacao_horario VARCHAR(5),
 	id_beneficiario INTEGER,  -- Adicionando a coluna para chave estrangeira
@@ -58,10 +58,17 @@ CREATE TABLE doacao (
 	doacao_qtd VARCHAR(50),
 	doacao_obs VARCHAR(100),
 	doacao_horario VARCHAR(5),
-  	doacao_data DATE,
 	id_doador INTEGER,  -- Adicionando a coluna para chave estrangeira
 	FOREIGN KEY (id_doador) REFERENCES cadastro_doador(id_doador)  -- Referência correta para chave estrangeira
 );
 
 SELECT * FROM doacao;
+
+ALTER TABLE solicitacao
+ADD COLUMN endereco_retirada VARCHAR(100);
+
+ALTER TABLE cadastro_beneficiario
+ADD COLUMN qtd_familiares int;
+
+
 
