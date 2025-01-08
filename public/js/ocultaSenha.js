23e4261a9b8a5e79dfb824bfcc931aa2ec27dbca
Kaiky Botelho de Faria <156143541+kaiky-botelho@users.noreply.github.com>
2025-01-08 14:38:17 -0300

    Update ocultaSenha.js

--- public/js/ocultaSenha.js
+++ public/js/ocultaSenha.js
@@ -1,5 +1,23 @@
+//Login
 document.getElementById('toggle-password').addEventListener('click', function() {
     const passwordField = document.getElementById('senha');
+    const eyeOpen = document.getElementById('eye-open');
+    const eyeClosed = document.getElementById('eye-closed');
+    
+    if (passwordField.type === 'password') {
+      passwordField.type = 'text'; // Altera o tipo do campo para "text"
+      eyeOpen.style.display = 'block';  // Exibe o ícone de olho aberto
+      eyeClosed.style.display = 'none'; // Esconde o ícone de olho fechado
+    } else {
+      passwordField.type = 'password'; // Altera o tipo do campo para "password"
+      eyeOpen.style.display = 'none';  // Esconde o ícone de olho aberto
+      eyeClosed.style.display = 'block'; // Exibe o ícone de olho fechado
+    }
+  });
+
+//Doador
+document.getElementById('toggle-password').addEventListener('click', function() {
+    const passwordField = document.getElementById('doador_senha');
     const eyeOpen = document.getElementById('eye-open');
     const eyeClosed = document.getElementById('eye-closed');
     
